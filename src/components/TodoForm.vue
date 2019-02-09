726d454380a290cf4fc1879990acb7826291c526
MrTj458 <mrtj458@gmail.com>
2019-02-08 22:55:08 -0700

    Fixed form refresh

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <form v-on:submit="addTodo">
+  <form v-on:submit.prevent="addTodo">
     <input v-model="title" type="text" placeholder="Add a Todo...">
     <button class="btn green" type="submit">Submit</button>
   </form>
